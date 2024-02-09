2108066aa2eb7c77d762ebcff553f43fbb31fe7a
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2024-02-09 11:00:45 -0800

    Connect to database with Sequelize

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,15 @@
-const Pool = require('pg').Pool;
+const Sequelize = require('sequelize');
 
-const pool = new Pool();
+//Connect to database
+const sequelize = new Sequelize(
+   process.env.DB_DATABASE, 
+   process.env.DB_USERNAME, 
+   process.env.DB_PASSWORD, 
+   {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: process.env.DB_DIALECT
+   }
+);
 
-exports.query = (text, params, callback) => {
-   return pool.query(text, params, callback);
-};
-
-exports.query = () => {
-   return pool.connect();
-};
+module.exports = sequelize;
