3bdd589889dfb5d45c9a894325f7c2694b32b9ec
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2024-03-14 11:28:06 -0700

    Differentiate earring and necklace IDs

--- client/src/app/_images/earringImageList.js
+++ client/src/app/_images/earringImageList.js
@@ -11,52 +11,52 @@ import e10 from './earrings/turtle-earrings.jpg';
 
 const earrings = [
    { 
-      id: 1, name: 'Blue Earrings', image: e1, price: 30, 
+      id: 'e1', name: 'Blue Earrings', image: e1, price: 30, 
       quantityInStock: 10, status: 'sale',
       description: 'Blue earrings - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Lacinia quis vel eros donec ac odio. Tortor dignissim convallis aenean et tortor at. Parturient montes nascetur ridiculus mus mauris vitae. Etiam erat velit scelerisque in dictum non consectetur a erat. Sagittis eu volutpat odio facilisis. Dictum non consectetur a erat nam at lectus. Erat nam at lectus urna duis. At risus viverra adipiscing at in. Tincidunt tortor aliquam nulla facilisi cras.'
    }, 
    { 
-      id: 2, name: 'Diamond Cluster Earrings', image: e2, price: 250, 
+      id: 'e2', name: 'Diamond Cluster Earrings', image: e2, price: 250, 
       quantityInStock: 3, status: '',
       description: 'Diamond Cluster earrings - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Lacinia quis vel eros donec ac odio. Tortor dignissim convallis aenean et tortor at. Parturient montes nascetur ridiculus mus mauris vitae. Etiam erat velit scelerisque in dictum non consectetur a erat. Sagittis eu volutpat odio facilisis. Dictum non consectetur a erat nam at lectus. Erat nam at lectus urna duis. At risus viverra adipiscing at in. Tincidunt tortor aliquam nulla facilisi cras.'
    }, 
    { 
-      id: 3, name: 'Diamond Earrings', image: e3, price: 500, 
+      id: 'e3', name: 'Diamond Earrings', image: e3, price: 500, 
       quantityInStock: 1, status: '',
       description: 'Diamond earrings - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Lacinia quis vel eros donec ac odio. Tortor dignissim convallis aenean et tortor at. Parturient montes nascetur ridiculus mus mauris vitae. Etiam erat velit scelerisque in dictum non consectetur a erat. Sagittis eu volutpat odio facilisis. Dictum non consectetur a erat nam at lectus. Erat nam at lectus urna duis. At risus viverra adipiscing at in. Tincidunt tortor aliquam nulla facilisi cras.'
    }, 
    { 
-      id: 4, name: 'Emerald Flower Earrings', image: e4, price: 85, 
+      id: 'e4', name: 'Emerald Flower Earrings', image: e4, price: 85, 
       quantityInStock: 2, status: 'sale',
       description: 'Emerald Flower earrings - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Lacinia quis vel eros donec ac odio. Tortor dignissim convallis aenean et tortor at. Parturient montes nascetur ridiculus mus mauris vitae. Etiam erat velit scelerisque in dictum non consectetur a erat. Sagittis eu volutpat odio facilisis. Dictum non consectetur a erat nam at lectus. Erat nam at lectus urna duis. At risus viverra adipiscing at in. Tincidunt tortor aliquam nulla facilisi cras.'
    }, 
    { 
-      id: 5, name: 'Gold Earrings', image: e5, price: 50, 
+      id: 'e5', name: 'Gold Earrings', image: e5, price: 50, 
       quantityInStock: 15, status: 'bestseller',
       description: 'Gold earrings - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Lacinia quis vel eros donec ac odio. Tortor dignissim convallis aenean et tortor at. Parturient montes nascetur ridiculus mus mauris vitae. Etiam erat velit scelerisque in dictum non consectetur a erat. Sagittis eu volutpat odio facilisis. Dictum non consectetur a erat nam at lectus. Erat nam at lectus urna duis. At risus viverra adipiscing at in. Tincidunt tortor aliquam nulla facilisi cras.'
    }, 
    { 
-      id: 6, name: 'Gold Twist Earrings', image: e6, price: 120, 
+      id: 'e6', name: 'Gold Twist Earrings', image: e6, price: 120, 
       quantityInStock: 8, status: 'new',
       description: 'Gold Twist earrings - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Lacinia quis vel eros donec ac odio. Tortor dignissim convallis aenean et tortor at. Parturient montes nascetur ridiculus mus mauris vitae. Etiam erat velit scelerisque in dictum non consectetur a erat. Sagittis eu volutpat odio facilisis. Dictum non consectetur a erat nam at lectus. Erat nam at lectus urna duis. At risus viverra adipiscing at in. Tincidunt tortor aliquam nulla facilisi cras.'
    }, 
    { 
-      id: 7, name: 'Pearl Circle Earrings', image: e7, price: 100, 
+      id: 'e7', name: 'Pearl Circle Earrings', image: e7, price: 100, 
       quantityInStock: 14, status: '',
       description: 'Pearl Circle earrings - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Lacinia quis vel eros donec ac odio. Tortor dignissim convallis aenean et tortor at. Parturient montes nascetur ridiculus mus mauris vitae. Etiam erat velit scelerisque in dictum non consectetur a erat. Sagittis eu volutpat odio facilisis. Dictum non consectetur a erat nam at lectus. Erat nam at lectus urna duis. At risus viverra adipiscing at in. Tincidunt tortor aliquam nulla facilisi cras.'
    }, 
    { 
-      id: 8, name: 'Pearl Earrings', image: e8, price: 50, 
+      id: 'e8', name: 'Pearl Earrings', image: e8, price: 50, 
       quantityInStock: 30, status: 'bestseller',
       description: 'Pearl earrings - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Lacinia quis vel eros donec ac odio. Tortor dignissim convallis aenean et tortor at. Parturient montes nascetur ridiculus mus mauris vitae. Etiam erat velit scelerisque in dictum non consectetur a erat. Sagittis eu volutpat odio facilisis. Dictum non consectetur a erat nam at lectus. Erat nam at lectus urna duis. At risus viverra adipiscing at in. Tincidunt tortor aliquam nulla facilisi cras.'
    }, 
    { 
-      id: 9, name: 'Silver Blue Flower Earrings', image: e9, price: 15, 
+      id: 'e9', name: 'Silver Blue Flower Earrings', image: e9, price: 15, 
       quantityInStock: 9, status: 'bestseller',
       description: 'Silver Blue Flower earrings - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Lacinia quis vel eros donec ac odio. Tortor dignissim convallis aenean et tortor at. Parturient montes nascetur ridiculus mus mauris vitae. Etiam erat velit scelerisque in dictum non consectetur a erat. Sagittis eu volutpat odio facilisis. Dictum non consectetur a erat nam at lectus. Erat nam at lectus urna duis. At risus viverra adipiscing at in. Tincidunt tortor aliquam nulla facilisi cras.'
    }, 
    { 
-      id: 10, name: 'Turtle Earrings', image: e10, price: 90, 
+      id: 'e10', name: 'Turtle Earrings', image: e10, price: 90, 
       quantityInStock: 6, status: 'new',
       description: 'Turtle earrings - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Lacinia quis vel eros donec ac odio. Tortor dignissim convallis aenean et tortor at. Parturient montes nascetur ridiculus mus mauris vitae. Etiam erat velit scelerisque in dictum non consectetur a erat. Sagittis eu volutpat odio facilisis. Dictum non consectetur a erat nam at lectus. Erat nam at lectus urna duis. At risus viverra adipiscing at in. Tincidunt tortor aliquam nulla facilisi cras.'
    }
