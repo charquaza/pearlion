b0e9a7e166e19454778ffd7ecf7e6c31cc49dc4f
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2024-04-26 17:31:53 -0700

    Import fonts

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,3 +1,6 @@
+import { 
+  Source_Sans_3, Hi_Melody, Mali, The_Girl_Next_Door, Bubbler_One 
+} from 'next/font/google';
 import Header from './_components/Header';
 import Sidebar from './_components/Sidebar';
 import Footer from './_components/Footer';
@@ -8,9 +11,29 @@ export const metadata = {
   description: "Jewelry shop",
 };
 
+const sourceSans3 = Source_Sans_3({
+  subsets: ['latin']
+});
+const hiMelody = Hi_Melody({
+  weight: '400',
+  subsets: ['latin']
+});
+const mali = Mali({
+  weight: '400',
+  subsets: ['latin']
+});
+const theGirlNextDoor = The_Girl_Next_Door({
+  weight: '400',
+  subsets: ['latin']
+});
+const bubblerOne = Bubbler_One({
+  weight: '400',
+  subsets: ['latin']
+});
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en-US">
+    <html lang="en-US" className={mali.className}>
       <body>
         <Header />
         <Sidebar />
