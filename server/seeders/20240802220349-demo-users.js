315ddb18f4d993f34c845f57e2ee08560aaa5c34
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2024-08-13 14:45:52 -0700

    Add seeder for demo users

---
+++ server/seeders/20240802220349-demo-users.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+const User = require('../models/index').User;
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await User.bulkCreate([
+      {
+        firstName: 'Admin',
+        lastName: 'Admin',
+        privilege: 'admin',
+        username: 'admin',
+        password: await bcrypt.hash('adminPass', 10),
+        email: 'admin@email.com'
+      },
+      {
+        firstName: 'Test',
+        lastName: 'User',
+        privilege: 'user',
+        username: 'testuser1',
+        password: await bcrypt.hash('userPass', 10),
+        email: 'testuser1@email.com'
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('users', { username: ['admin', 'testuser1'] });
+  }
+};
