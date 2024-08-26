c635e558dd15956b097d607616469b606f5b8880
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2024-08-26 15:37:49 -0700

    Connect login UI to api

--- client/src/app/_components/LoginFormPopover.js
+++ client/src/app/_components/LoginFormPopover.js
@@ -1,24 +1,61 @@
 'use client';
 
 import { useState } from 'react';
+import { apiURL } from '@/root/config';
 import styles from '@/app/_styles/LoginFormPopover.module.css';
 
-export default function LoginFormPopover() {
+export default function LoginFormPopover({ setShowLoginPopover, revalidateCurrUser }) {
    const [ inputValues, setInputValues ] = useState({
       username: '', 
       password: ''
    });
+   const [ loginError, setLoginError ] = useState(null);
 
-   function handleLogin(e) {
+   async function handleLogin(e) {
       e.preventDefault();
+
+      try {
+         const fetchOptions = {
+            method: 'POST',
+            headers: { 
+               'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(inputValues),
+            mode: 'cors',
+            credentials: 'include',
+            cache: 'no-store'
+         };
+         const fetchURL = apiURL + '/users/log-in';
+   
+         const res = await fetch(fetchURL, fetchOptions);
+   
+         if (res.ok) {
+            setShowLoginPopover(false);
+            revalidateCurrUser();
+            setLoginError(null);
+         } else if (res.status === 401) {
+            const data = await res.json();
+            setLoginError(data.errors[0]);
+         } else {
+            setLoginError('Unable to log in at this time, please try again later');
+         }
+      } catch (e) {
+         setLoginError('Unable to log in at this time, please try again later');
+         console.error('Error message: ' + e);
+      }
    }
 
    function handleInput(e) {
-      setInputValues({ ...inputValues, [e.target.name]: [e.target.value] });
+      setInputValues({ ...inputValues, [e.target.name]: e.target.value });
    }
 
    return (
       <form onSubmit={handleLogin} className={styles['login-form']}>
+         {
+            loginError &&
+               <p className={styles['error-msg']}>{loginError}</p>
+         }
+
          <label htmlFor='username'>Username:&nbsp;</label>
          <input type='text' name='username' id='username'
             required value={inputValues.username}
--- client/src/app/_components/Topbar.js
+++ client/src/app/_components/Topbar.js
@@ -1,15 +1,32 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import useSWR from 'swr';
 import Link from 'next/link';
 import LoginFormPopover from './LoginFormPopover';
+import { apiURL } from '@/root/config';
 import styles from '@/app/_styles/Topbar.module.css';
 
 export default function Topbar() {
    const [ showLoginPopover, setShowLoginPopover ] = useState(false);
 
+   const { data: userData, error, isLoading, mutate } = useSWR(apiURL + '/users/curr-user', url => {
+      const fetchOptions = {
+         method: 'GET',
+         mode: 'cors',
+         credentials: 'include',
+         cache: 'no-store'
+      };
+
+      return fetch(url, fetchOptions).then(res => res.json());
+   }, { shouldRetryOnError: false });
+
    useEffect(function addEventListeners() {
       //do not mix native browser events with React's synthetic events
+
+      if (userData && userData.data) { //attach listeners if no user is logged in
+         return;
+      }
 
       function toggleLoginPopover(e) {
          setShowLoginPopover(prev => !prev);
@@ -34,37 +51,80 @@ export default function Topbar() {
          popover.removeEventListener('click', handlePopoverClick, false);
          document.body.removeEventListener('click', closeLoginPopover, false);
       };
-   }, []);
+   }, [userData]);
+
+   async function handleLogOut(e) {
+      try {
+         const fetchOptions = {
+            method: 'POST',
+            mode: 'cors',
+            credentials: 'include'
+         };
+         const fetchURL = apiURL + '/users/log-out';
+
+         const res = await fetch(fetchURL, fetchOptions);
+         
+         if (res.ok) {
+            mutate();
+         } else {
+            const data = await res.json();
+            console.error(data);
+            alert('Logout unsuccessful - please try again later');
+         }
+      } catch (err) {
+         console.error(err);
+         alert('Logout unsuccessful - please try again later');
+      }
+   }
 
    return (
       <nav className={styles['topbar']}>
          <ul className={styles['nav-list']}>
-            <li>
-               <div className={styles['popover-control']}>
-                  <button 
-                     id='login-button'
-                  >
-                     { showLoginPopover ? 'Close' : 'Log In' }
-                  </button>
-   
-                  <div 
-                     className={
-                        showLoginPopover 
-                           ? styles['login-popover-open']
-                           : styles['login-popover-closed']
-                     }
-                     id='popover'
-                  >
-                     {
-                        showLoginPopover &&
-                           <LoginFormPopover />
-                     }
-                  </div>
-               </div>            
-            </li>
-            <li>
-               <Link href='/sign-up'>Sign Up</Link>
-            </li>
+            {
+               (userData && !userData.errors) 
+                  ?
+                     <>
+                        <li>
+                           <Link href='/account'>My Account</Link>
+                        </li>
+                        <li>
+                              <button onClick={handleLogOut}>Logout</button>
+                        </li>
+                     </>
+                  :
+                     <>
+                        <li>
+                           <div className={styles['popover-control']}>
+                              <button 
+                                 id='login-button'
+                              >
+                                 { showLoginPopover ? 'Close' : 'Log In' }
+                              </button>
+               
+                              <div 
+                                 className={
+                                    showLoginPopover 
+                                       ? styles['login-popover-open']
+                                       : styles['login-popover-closed']
+                                 }
+                                 id='popover'
+                              >
+                                 {
+                                    showLoginPopover &&
+                                       <LoginFormPopover 
+                                          setShowLoginPopover={setShowLoginPopover} 
+                                          revalidateCurrUser={mutate}
+                                       />
+                                 }
+                              </div>
+                           </div>            
+                        </li>
+                        <li>
+                           <Link href='/sign-up'>Sign Up</Link>
+                        </li>
+                     </>
+            }
+
             <li>
                <Link href='/cart' aria-label='shopping cart'>🛒</Link>
             </li>
