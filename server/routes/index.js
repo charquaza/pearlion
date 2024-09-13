25d88141d12257e02bf3e085adf6b900364f4cee
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2024-09-13 15:46:44 -0700

    Add Review routes to index router

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const userRouter = require('./user');
 const productRouter = require('./product');
 const orderRouter = require('./order');
+const reviewRouter = require('./review');
 
 router.use('/users', userRouter);
 router.use('/products', productRouter);
 router.use('/orders', orderRouter);
+router.use('/reviews', reviewRouter);
 
 module.exports = router;
