0262472a3276a5c8042562dd3ed87f23d5772c08
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2024-09-25 14:05:51 -0700

    Add hook for fetching Product list

---
+++ client/src/app/_hooks/useProductList.js
@@ -0,0 +1,46 @@
+import useSWR from 'swr';
+import { apiURL } from '@/root/config'; 
+
+export default function useProductList(category, status, images) {
+   let urlParams = new URLSearchParams({
+      ...(category && { category }),
+      ...(status && { status }),
+      ...(images && { images })
+   });
+
+   let requestURL = new URL('products', apiURL);
+   requestURL.search = urlParams.toString();
+   requestURL = requestURL.toString();
+
+   const { data, error, isLoading, mutate } = useSWR(
+      requestURL, 
+      async url => {
+         const fetchOptions = {
+            method: 'GET',
+            mode: 'cors',
+            credentials: 'include',
+            cache: 'no-store'
+         };
+
+         const res = await fetch(url, fetchOptions);
+         const data = await res.json();
+
+         if (data.errors) {
+            let error = new Error('Error occurred while fetching product data');
+            error.info = data.errors;
+            error.status = res.status;
+            throw error;
+         } else {
+            return data;
+         }
+      }, 
+      { errorRetryCount: 3, revalidateOnFocus: false }
+   );
+
+   return { 
+      productList: data, 
+      error, 
+      isLoading, 
+      mutate 
+   };
+};
