b1be22cef5af2952b101fc8388f3724a227de229
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2024-10-01 19:28:17 -0700

    Edit styling for ReviewCard

--- client/src/app/_components/ReviewCard.js
+++ client/src/app/_components/ReviewCard.js
@@ -1,3 +1,4 @@
+import { DateTime } from 'luxon';
 import RatingBar from './RatingBar';
 import ReviewImageList from './ReviewImageList';
 import styles from '@/app/_styles/ReviewCard.module.css';
@@ -5,15 +6,15 @@ import styles from '@/app/_styles/ReviewCard.module.css';
 export default function ReviewCard({ productName, review, reviewIndex, toggleReviewPopover }) {
    return (
       <article className={styles['review']}>
-         <h3>{review.client}</h3>
-         <p>{review.date}</p>
-         <p>Product Reviewed: {productName}</p>
+         <h3>{review.User.firstName + ' ' + review.User.lastName}</h3>
          <RatingBar reviewCount={1} ratingSum={review.rating} context='review-card' />
+         <p>{DateTime.fromISO(review.createdAt).toLocaleString(DateTime.DATE_MED)}</p>
+         <p>Product Reviewed: {productName}</p>
          <ReviewImageList review={review} 
             reviewIndex={reviewIndex}
             toggleReviewPopover={toggleReviewPopover}
          />
-         <p>{review.review}</p>
+         <p className={styles['review-text-body']}>{review.review}</p>
       </article>
    );
 };
