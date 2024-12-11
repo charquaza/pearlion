dda46422c08879f82e77c896e13df5d3fdd9aaab
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2024-12-11 08:57:24 -0800

    Close Sidebar after click on nav link

--- client/src/app/_components/Sidebar.js
+++ client/src/app/_components/Sidebar.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import styles from '@/app/_styles/Sidebar.module.css';
@@ -9,12 +9,31 @@ export default function Sidebar() {
    const [ menuOpen, setMenuOpen ] = useState(false);
    const pathname = usePathname();
 
+   // useEffect(function addEventListeners() {
+   //    function closeMenu(e) {
+   //       setMenuOpen(false);
+   //    }
+   //    function handleSidebarClick(e) {
+   //       e.stopPropagation();
+   //    }
+
+   //    const sidebar = document.getElementById('sidebar');
+
+   //    document.body.addEventListener('click', closeMenu, false);
+   //    sidebar.addEventListener('click', handleSidebarClick, false);
+
+   //    return () => {
+   //       document.body.removeEventListener('click', closeMenu, false);
+   //       sidebar.removeEventListener('click', handleSidebarClick, false);
+   //    };
+   // }, [setMenuOpen]);
+
    function handleMenuToggle(e) {
       setMenuOpen(prev => !prev);
    }
 
    return (
-      <nav className={styles.sidebar}>
+      <nav className={styles.sidebar} id={'sidebar'}>
          {menuOpen 
             ?
                <button className={styles['menu-icon-close']} onClick={handleMenuToggle}
@@ -26,35 +45,37 @@ export default function Sidebar() {
                >&equiv;</button>
          }
       
-         <ul className={
-            menuOpen ? styles['nav-list'] : (styles['nav-list'] + ' ' + styles['mobile-hidden']) 
-         }>
-            <li>
+         <ul 
+            className={
+               menuOpen ? styles['nav-list'] : (styles['nav-list'] + ' ' + styles['mobile-hidden']) 
+            }
+         >
+            <li onClick={handleMenuToggle}>
                <Link href='/about-us' 
                   className={pathname === '/about-us' ? styles['current-route'] : undefined}
                >About Us</Link>
             </li>
-            <li>
+            <li onClick={handleMenuToggle}>
                <Link href='/new-arrivals'
                   className={pathname === '/new-arrivals' ? styles['current-route'] : undefined}
                >New Arrivals</Link>
             </li>
-            <li>
+            <li onClick={handleMenuToggle}>
                <Link href='/best-sellers'
                   className={pathname === '/best-sellers' ? styles['current-route'] : undefined}
                >Best Sellers</Link>
-               </li>
-            <li>
+            </li>
+            <li onClick={handleMenuToggle}>
                <Link href='/earrings'
                   className={pathname === '/earrings' ? styles['current-route'] : undefined}
                >Earrings</Link>
             </li>
-            <li>
+            <li onClick={handleMenuToggle}>
                <Link href='/necklaces'
                   className={pathname === '/necklaces' ? styles['current-route'] : undefined}
                >Necklaces</Link>
             </li>
-            <li>
+            <li onClick={handleMenuToggle}>
                <Link href='/sale'
                   className={pathname === '/sale' ? styles['current-route'] : undefined}
                >Sale</Link>
