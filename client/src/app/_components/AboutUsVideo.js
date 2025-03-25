b480590265009707f1fedcc2a4a4175f5c08722b
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2025-03-24 18:20:30 -0700

    Get About Us video url from environment variable

--- client/src/app/_components/AboutUsVideo.js
+++ client/src/app/_components/AboutUsVideo.js
@@ -1,7 +1,7 @@
 export default function AboutUsVideo() {
    return (
       <iframe 
-         src="https://www.youtube.com/embed/Pd1ZpAfpV4g?si=cqHpPAYd9QwbnVGY" 
+         src={process.env.NEXT_PUBLIC_ABOUT_US_VIDEO_URL} 
          title="About Us video" frameborder="0" 
          allow="accelerometer; autoplay; clipboard-write; encrypted-media; fullscreen; gyroscope; picture-in-picture; web-share" 
          allowfullscreen
